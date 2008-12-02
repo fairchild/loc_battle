dddb58408bc3597507408e3fade2503ad99ed4d5
Michael <fairchild@stimble.net>
2008-12-01 20:20:17 -0800

    commit the first file

---
+++ loc_battle.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/vendor/'
+SINATRA_ROOT=File.dirname(__FILE__)
+
+configure do
+  # Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://loc_battle.db')
+end
+
+### Public
+
+get '/' do
+  @contenders = Dir.entries('contenders')
+  erb :contenders
+end
+
+get '/contender/:name' do
+  @contender = params[:name]
+  @command = "cloc.pl -no3 --quiet contenders/#{@contender} --report-file=public/results/#{@contender}"
+  @stats = `#{@command}` if !File.exist?("public/results/#{@contender}")
+  erb :contender
+end
+
+
